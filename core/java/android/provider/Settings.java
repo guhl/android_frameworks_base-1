2c8fc4510388be3d5e39dae63dfd1bbd07dd0b8e
guhl <guhl99@yahoo.com>
2012-11-23 20:49:20 +0100

    pff: Infrastructure needed for permission spoofing
    - Based on the original work by Plamen K. Kosseff
    - Initial commit
    
    Change-Id: Idb6de41a9d178c98a123974dc2071de6896783f8
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/public.xml
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3513,7 +3513,7 @@ public final class Settings {
          * constant for the lifetime of the device.  (The value may
          * change if a factory reset is performed on the device.)
          */
-        public static final String ANDROID_ID = "android_id";
+       public static final String ANDROID_ID = "android_id";
 
         /**
          * @deprecated Use {@link android.provider.Settings.Global#BLUETOOTH_ON} instead
@@ -4461,6 +4461,13 @@ public final class Settings {
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
         /**
+         * Guhl:
+         * Whether to enable permissions managment.
+	 * @hide
+	 */
+	public static final String ENABLE_PERMISSIONS_MANAGEMENT = "enable_permissions_management";       
+
+	/**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -4561,6 +4568,8 @@ public final class Settings {
             putStringForUser(cr, Settings.Secure.LOCATION_PROVIDERS_ALLOWED, provider,
                     userId);
         }
+        
+        
     }
 
     /**
@@ -6028,7 +6037,6 @@ public final class Settings {
          * always stored as strings; this function converts the string to an
          * float for you. The default value will be returned if the setting
          * is not defined or not a valid float.
-         *
          * @param cr The ContentResolver to access.
          * @param name The name of the setting to retrieve.
          * @param def Value to return if the setting is not defined.
