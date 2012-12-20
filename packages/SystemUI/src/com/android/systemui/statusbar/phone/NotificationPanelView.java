f389f4ae0be2295a4bdd76ad4dfcd243f3bd57a4
Rick C <rickfitsnugly@gmail.com>
2012-12-19 23:07:42 -0500

    Quick Settings Pulldown: Left or Right side option (1/2)
    Patch set 2 : Fix defaults from upgrading
    Patch set 3 : Change percentages
    
    Change-Id: I1f74043865e2670430da7c90ddc8aacc8e5267be
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -110,9 +110,13 @@ public class NotificationPanelView extends PanelView {
             switch (event.getActionMasked()) {
                 case MotionEvent.ACTION_DOWN:
                     mOkToFlip = getExpandedHeight() == 0;
-                    if (event.getX(0) > getWidth() * (1.0f - STATUS_BAR_SETTINGS_FLIP_PERCENTAGE) &&
+                    if (event.getX(0) > getWidth() * (1.0f - STATUS_BAR_SETTINGS_RIGHT_PERCENTAGE) &&
                             Settings.System.getInt(getContext().getContentResolver(),
-                                    Settings.System.QS_QUICK_PULLDOWN, 0) != 0) {
+                                    Settings.System.QS_QUICK_PULLDOWN, 0) == 1) {
+                        flip = true;
+                    } else if (event.getX(0) < getWidth() * (1.0f - STATUS_BAR_SETTINGS_LEFT_PERCENTAGE) &&
+                            Settings.System.getInt(getContext().getContentResolver(),
+                                    Settings.System.QS_QUICK_PULLDOWN, 0) == 2) {
                         flip = true;
                     }
                     break;
