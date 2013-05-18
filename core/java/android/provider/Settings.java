d5f8814dd6cbf5de849b7805bfb70823e0bbe5be
Paul Beeler <sparksco@gmail.com>
2013-05-18 15:15:38 -0600

    Revert "Launch music player on headset connect"
    This reverts commit b72bce257a65540462c0103c6d7a3fd0ada40d2d.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2465,12 +2465,6 @@ public final class Settings {
         public static final String NAV_BUTTONS = "nav_buttons";
 
         /**
-         * Whether or not to launch default music player when headset is connected
-         * @hide
-         */
-        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
-
-        /**
          * Height of navigation bar buttons
          * @hide
          */
